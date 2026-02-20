283f6299c61edb451dc133886148eb27f28ce4a9
Ryan Zheng <rzheng1232@gmail.com>
2026-02-20 01:01:00 -0600

    Turned code into platformio project for native vscode editing. Established data channel between arduino and computer through low power bluetooth and able to recieve 4 channels of decimal values. Added live matplotlib signal visualizer (really unoptimized)

--- arduino_data_collection/src/arduino.ino
+++ arduino_data_collection/src/arduino.ino
@@ -1,30 +1,29 @@
 #include <ArduinoBLE.h>
 
 BLEService msgService("12345678-1234-1234-1234-1234567890ab");
-BLEStringCharacteristic msgChar(
+BLECharacteristic msgChar(
   "abcdefab-1234-5678-1234-abcdefabcdef",
   BLERead | BLENotify,
-  20
+  16
 );
 
 unsigned long lastSend = 0;
 int counter = 0;
+float emgData[4] = {0.013, 0.321, 0.6767, 0.676767};
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
-
+  // while (!Serial);
   if (!BLE.begin()) {
     while (1);
   }
 
-  BLE.setLocalName("Uno R4 Test");
+  BLE.setLocalName("SigRoboArd");
   BLE.setAdvertisedService(msgService);
 
   msgService.addCharacteristic(msgChar);
   BLE.addService(msgService);
-
-  msgChar.writeValue("0");
+  msgChar.writeValue((byte*)emgData, sizeof(emgData));
 
   BLE.advertise();
 }
@@ -33,11 +32,12 @@ void loop() {
   BLEDevice central = BLE.central();
 
   if (central) {
+    // send something every 1000 ms
     while (central.connected()) {
       if (millis() - lastSend >= 1000) {
         lastSend = millis();
 
-        msgChar.writeValue(String(counter));
+        msgChar.writeValue((uint8_t*)emgData, sizeof(emgData));
         counter++;
       }
 
